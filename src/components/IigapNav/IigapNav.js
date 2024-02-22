0aa361891096625f689d26ca149ddbe303267775
rinomarra <62858445+rinomarra@users.noreply.github.com>
2024-02-22 15:49:01 +0100

    add file ugo

--- src/components/IigapNav/IigapNav.js
+++ src/components/IigapNav/IigapNav.js
@@ -5,7 +5,7 @@ import { FaSearch } from 'react-icons/fa';
 import useSite from 'hooks/use-site';
 import useSearch, { SEARCH_STATE_LOADED } from 'hooks/use-search';
 import { postPathBySlug } from 'lib/posts';
-import { findMenuLocationById, findMenuByLocation, MENU_LOCATION_NAVIGATION_DEFAULT } from 'lib/menus';
+import { findMenuLocationById, findMenuByLocation, MENU_LOCATION_NAVIGATION_DEFAULT} from 'lib/menus';
 
 import Section from 'components/Section';
 
@@ -15,8 +15,11 @@ import Oxygen from 'components/Oxygen';
 const SEARCH_VISIBLE = 'visible';
 const SEARCH_HIDDEN = 'hidden';
 
-const IigapNav = ({ idMenu }) => {
+const IigapNav = ({idMenu}) => {
   const formRef = useRef();
+
+
+  
 
   const [searchVisibility, setSearchVisibility] = useState(SEARCH_HIDDEN);
 
@@ -25,27 +28,89 @@ const IigapNav = ({ idMenu }) => {
 
   const location = findMenuLocationById(menus, idMenu);
   const navigation = findMenuByLocation(menus, location);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const genericHamburgerLine = `h-0.5 w-8 my-1 transition ease transform duration-300`;
+  const genericNav = 'z-40 fixed left-0 top-0 bg-white w-full h-screen transition ease transform duration-300';
+  if(location == 'NEXT_MAIN_MENU') {
   return (
-    <nav className="fixed left-0 top-0 bg-white w-full h-screen flex">
-      <div className="w-1/2">
+    <>
+    <button
+        className="flex flex-col h-12 w-12 justify-center items-center group z-50"
+        onClick={() => {
+          setIsOpen(!isOpen);
+          document.querySelector('body').classList.toggle('locked');
+        }}
+    >
+        <span
+            className={`${genericHamburgerLine} ${
+                isOpen
+                    ? "rotate-45 translate-y-2 group-hover:opacity-100 bg-black"
+                    : "group-hover:opacity-100 bg-white"
+            }`}
+        />
+        <span className={`${genericHamburgerLine}
+          ${isOpen
+            ? "opacity-0 bg-black"
+            : " group-hover:opacity-100 bg-white"
+          }`}
+        />
+        <span
+            className={`${genericHamburgerLine} ${
+                isOpen
+                    ? "-rotate-45 -translate-y-3 group-hover:opacity-100 bg-black"
+                    : "group-hover:opacity-100 bg-white"
+            }`}
+        />
+    </button>
+    <nav id={ location.toLowerCase()} className={`${genericNav} ${isOpen ? "flex" : "collapse"}`}> 
+        <div className='w-1/3'>
+          
+        </div>
+        <div id="menu-modules" className='w-1/3 relative'>
         {navigation?.map((listItemFirst) => {
-          return <Oxygen className="hidden" key={listItemFirst.id} json={listItemFirst.menuModule} />;
+          
+          return(
+              <div id={"menu-"+listItemFirst.databaseId} className="invisible bg-red-50 menu-module absolute">
+              <Oxygen key={listItemFirst.id} json={listItemFirst.menuModule} />
+              </div>
+          );
         })}
       </div>
-      <div className="w-1/2">
-        <ul
-          // set style to justify-content: space-between
-          style={{ justifyContent: 'space-evenly' }}
-          className="h-full flex flex-col p-28"
+      <div className='w-1/3'>
+        <ul 
+        // set style to justify-content: space-between
+        style={{justifyContent: 'space-evenly'}}
+        className="h-full flex flex-col p-28"
         >
-          {navigation?.map((listItem) => {
-            return <NavListItem key={listItem.id} item={listItem} />;
+        {navigation?.map((listItem) => {
+            
+            return(
+                <NavListItem  key={listItem.id} item={listItem} />
+            );
           })}
         </ul>
       </div>
     </nav>
+    </>
+
   );
+  }
+  else{
+    return (
+      <ul 
+      // set style to justify-content: space-between
+      style={{justifyContent: 'space-evenly'}}
+      className="h-full flex flex-col p-28"
+      >
+      {navigation?.map((listItem) => {
+          
+          return(
+              <NavListItem  key={listItem.id} item={listItem} />
+          );
+        })}
+      </ul>
+    )
+  }
 };
 
 export default IigapNav;
--- src/components/NavListItem/NavListItem.js
+++ src/components/NavListItem/NavListItem.js
@@ -1,14 +1,35 @@
 // import ClassName from 'models/classname';
 // import styles from './NavListItem.module.scss';
 import Link from 'next/link';
-
+import { gsap } from 'gsap';
 const NavListItem = ({ className, item }) => {
   const nestedItems = (item.children || []).map((item) => {
     return <NavListItem key={item.id} item={item} />;
   });
 
+  const manageMouseEnter = (e) => {
+    console.log(e.target);
+    const targetId = e.target.getAttribute('data-module');
+    gsap.to(`#${targetId}`, {visibility: 'visible', opacity:  1, duration:  0.3});
+  };
+  
+  const manageMouseLeave = (e) => {
+
+    // set all #menu-modules child to hidden
+    gsap.to('.menu-module', {visibility: 'hidden', opacity:  0, duration:  0.3});
+    const targetId = e.target.getAttribute('data-module');
+    gsap.to(`#${targetId}`, {visibility: 'hidden', opacity:  0, duration:  0.3});
+  };
+
   return (
-    <li data-module={'menu-' + item.databaseId} key={item.id}>
+    <li
+      // on mouse enter, set the visibility of the target to visible
+      onMouseEnter={manageMouseEnter}
+      // on mouse leave, set the visibility of the target to hidden
+      onMouseLeave={manageMouseLeave}
+      data-module={'menu-'+item.databaseId}
+      key={item.id}
+    >
       {/* 
         Before rendering the Link component, we first check if `item.path` exists
         and if it does not include 'http'. This prevents a TypeError when `item.path` is null.
